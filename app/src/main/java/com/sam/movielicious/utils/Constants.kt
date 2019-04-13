2ebb296a46544bfc761b29b62b2b70618e67cb84
HossamOnsy <HossamEldeenOnsy@yahoo.com>
2019-04-13 18:39:31 +0200

    Changing Images

--- app/src/main/java/com/sam/movielicious/utils/Constants.kt
+++ app/src/main/java/com/sam/movielicious/utils/Constants.kt
@@ -4,7 +4,6 @@ package com.sam.movielicious.utils
 
 const val API_KEY: String = "<ENTER YOUR API KEY HERE>"
 
-
 const val BASE_URL: String = "https://api.themoviedb.org"
 
 const val POSTERPATHW185 : String = "https://image.tmdb.org/t/p/w185/"
