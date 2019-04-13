962e835e0aeb703bafc5529cbd3aefc210d544c2
HossamOnsy <HossamEldeenOnsy@yahoo.com>
2019-04-13 18:33:23 +0200

    Adding Spl

--- app/src/main/java/com/sam/movielicious/utils/Constants.kt
+++ app/src/main/java/com/sam/movielicious/utils/Constants.kt
@@ -4,8 +4,8 @@ package com.sam.movielicious.utils
 
 const val API_KEY: String = "<ENTER YOUR API KEY HERE>"
 
-const val BASE_URL: String = "https://api.themoviedb.org"
 
+const val BASE_URL: String = "https://api.themoviedb.org"
 
 const val POSTERPATHW185 : String = "https://image.tmdb.org/t/p/w185/"
 const val POSTERPATHORIGINAL : String = "https://image.tmdb.org/t/p/w500/"
